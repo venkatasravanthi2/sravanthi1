453badc44240de0de2139cbf8b766f23b1b64a31
venkatasravanthi2 <41746796+venkatasravanthi2@users.noreply.github.com>
2018-08-17 15:40:22 +0530

    Update amstrong.py

--- amstrong.py
+++ amstrong.py
@@ -1,6 +1,7 @@
-a,m=input().split()
-a,m=int(a),int(m)
-for i in range(a+1,m):
+n,m=input().split()
+n,m=int(n),int(m)
+list=[]
+for i in range(n+1,m):
     temp=i
     sum=0
     while(i>0):
@@ -8,5 +9,12 @@ for i in range(a+1,m):
         sum=sum+rem**3
         i=i//10
     if(temp==sum):
-        print(temp,end=' ')
+        list.append(temp)
+for i in range(0,len(list)):
+    if (i<len(list)-1):
+        k=' '
+    else:
+        k=''
+    print(list[i],end=k)
+
 
